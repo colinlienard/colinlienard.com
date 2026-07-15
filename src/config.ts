85159a772b92d37bd492dfc7d6f248e95eeaaba4
Colin Lienard <colin.lienard87@gmail.com>
2026-07-15 23:18:37 +0200

    feat: multiple roles per experience

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 export const CONTACT = {
 	email: 'hello@colinlienard.com',
-	cal: 'https://cal.com/colinlienard/30min',
 	linkedin: 'https://www.linkedin.com/in/colin-lienard/',
 	malt: 'https://www.malt.fr/profile/colinlienard',
 	github: 'https://github.com/colinlienard',
